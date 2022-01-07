b767fd0dd9b80eb1415f4b82924002d846549ee1
Lizaveta <lizadondysh@icloud.com>
2022-01-07 18:14:24 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let unsortedString = Math.abs(n).toString();
+    let unsortedArray = unsortedString.split('');
+    let finalNumber = unsortedArray.reverse().join('');
+    return finalNumber;
 }
